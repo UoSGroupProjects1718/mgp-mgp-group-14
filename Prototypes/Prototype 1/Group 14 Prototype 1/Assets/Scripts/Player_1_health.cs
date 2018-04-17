3a77bdccc8ee6096a69d30b25aae4f5227528963
E-Ward <s182964@ucs.ac.uk>
2018-04-17 23:39:47 +0100

    Worked on game and presentation

--- Prototypes/Prototype 1/Group 14 Prototype 1/Assets/Scripts/Player_1_health.cs
+++ Prototypes/Prototype 1/Group 14 Prototype 1/Assets/Scripts/Player_1_health.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 public class Player_1_health : MonoBehaviour {
     
     public GameObject Attackref;
-	
+
+	public Slider slider1;
+
     void Update ()
     {
         Attack attackScript = Attackref.GetComponent<Attack>();
         Text Health_Display = GetComponent<Text>();
         Health_Display.text = attackScript.player1_hp.ToString();
+
+		slider1.value = attackScript.player1_hp;
     }
+		
 }
--- Prototypes/Prototype 1/Group 14 Prototype 1/Assets/Scripts/SpawnPendulum.cs
+++ Prototypes/Prototype 1/Group 14 Prototype 1/Assets/Scripts/SpawnPendulum.cs
@@ -6,57 +6,142 @@ public class SpawnPendulum : MonoBehaviour {
     public GameObject pendulum;
     public GameObject AttackRef;
     public bool check = false; //Just a check in the logic of the speedup
+    public bool check2 = false; //Check variable for player2
+    public bool ChangeSpeed = false;
+    public bool ChangeSpeed2 = false; 
+
+    
+    public void Update()
+    {
+        Attack atkref = AttackRef.GetComponent<Attack>();
+        if (check == true && (atkref.currentPlayer == 2 || atkref.currentPlayer == 4))
+        {
+            check = false;
+            ChangeSpeed = true;
+        }
+
+        if (check2 == true && (atkref.currentPlayer == 1 || atkref.currentPlayer == 5))
+        {
+            check2 = false;
+            ChangeSpeed2 = true;
+        }
+    }
+    public void SpawnAttack()
+    {
+        Vector3 Position = new Vector3(0, 0.89f, -8.66f);
+        pendulum = Instantiate(Resources.Load("Pendulum_Attack"), Position, Quaternion.identity) as GameObject;
+        pendulum.name = "Pendulum1";
+        Movement_Attack_Dodge reference = pendulum.GetComponentInChildren<Movement_Attack_Dodge>();
+        if (ChangeSpeed)
+        {
+            reference.speed = 2.5f;
+            ChangeSpeed = false;
+        }
+        else
+        {
+            reference.speed = Random.Range(1.0f, 1.7f);
+        }
+        if (ChangeSpeed2)
+        {
+            reference.speed = 2.5f;
+            ChangeSpeed2 = false;
+        }
+        else
+        {
+            reference.speed = Random.Range(1.0f, 1.7f);
+        }
+    }
+
+    public void SpawnDodge()
+    {
+        Vector3 Position = new Vector3(0, 0.89f, -8.66f);
+        pendulum = Instantiate(Resources.Load("Pendulum_Dodge"), Position, Quaternion.identity) as GameObject;
+        pendulum.name = "Pendulum1";
+        Movement_Attack_Dodge reference = pendulum.GetComponentInChildren<Movement_Attack_Dodge>();
+        if (ChangeSpeed)
+        {
+            reference.speed = 2.5f;
+            ChangeSpeed = false;
+        }
+        else
+        {
+            reference.speed = Random.Range(1.0f, 1.7f);
+        }
+        if (ChangeSpeed2)
+        {
+            reference.speed = 2.5f;
+            ChangeSpeed2 = false;
+        }
+        else
+        {
+            reference.speed = Random.Range(1.0f, 1.7f);
+        }
+    }
+
+    public void SpawnHeal()
+    {
+        Vector3 Position = new Vector3(0, 0.89f, -8.66f);
+        pendulum = Instantiate(Resources.Load("Pendulum_Heal"), Position, Quaternion.identity) as GameObject;
+        pendulum.name = "Pendulum1";
+        Movement_Attack_Dodge reference = pendulum.GetComponentInChildren<Movement_Attack_Dodge>();
+        if (ChangeSpeed)
+        {
+            reference.speed = 2.5f;
+            ChangeSpeed = false;
+        }
+        else
+        {
+            reference.speed = Random.Range(1.0f, 1.7f);
+        }
+        if (ChangeSpeed2)
+        {
+            reference.speed = 2.5f;
+            ChangeSpeed2 = false;
+        }
+        else
+        {
+            reference.speed = Random.Range(1.0f, 1.7f);
+        }
+    }
+
+    public void SpawnSpeed()
+    {
+        Vector3 Position = new Vector3(0, 0.89f, -8.66f);
+        pendulum = Instantiate(Resources.Load("Pendulum_Speed"), Position, Quaternion.identity) as GameObject;
+        pendulum.name = "Pendulum1";
+        Movement_Attack_Dodge reference = pendulum.GetComponentInChildren<Movement_Attack_Dodge>();
+        if (ChangeSpeed)
+        {
+            reference.speed = 2.5f;
+            ChangeSpeed = false;
+            
+        }
+        else
+        {
+            reference.speed = Random.Range(1.0f, 1.7f);
+        }
+        if (ChangeSpeed2)
+        {
+            reference.speed = 2.5f;
+            ChangeSpeed2 = false;
+        }
+        else
+        {
+            reference.speed = Random.Range(1.0f, 1.7f);
+        }
+    }
 
     public void Spawn()
     {
-        Vector3 Position = new Vector3(0, 0.89f, -8.66f);
-
-        
-        pendulum = Instantiate(Resources.Load("Pendulum"), Position, Quaternion.identity) as GameObject;
-        pendulum.name = "Pendulum1";
         Attack atkref = AttackRef.GetComponent<Attack>();
-
-        if (atkref.SpeedUpPendP1 == true && atkref.currentPlayer == 1)
+        
+        if (atkref.currentPlayer == 1 || atkref.currentPlayer == 5)
         {
-            if (check == true)
-            {
-                Movement_Attack_Dodge reference = pendulum.GetComponentInChildren<Movement_Attack_Dodge>();
-                reference.speed = 1.5f;
-                check = false;
-            }
-            else if (check == false)
-            {
-                Movement_Attack_Dodge reference = pendulum.GetComponentInChildren<Movement_Attack_Dodge>();
-                reference.speed = 2.5f;
-                atkref.SpeedUpPendP1 = false;
-            }
+            check = true;
         }
-        else if (atkref.SpeedUpPendP1 == true && atkref.currentPlayer == 2)
+        else if (atkref.currentPlayer == 2 || atkref.currentPlayer == 4)
         {
-            Movement_Attack_Dodge reference = pendulum.GetComponentInChildren<Movement_Attack_Dodge>();
-            reference.speed = 2.5f;
-            atkref.SpeedUpPendP1 = false;
-        }
-        else if (atkref.SpeedUpPendP1 == false && (atkref.currentPlayer == 1 || atkref.currentPlayer == 2))
-        {
-            Movement_Attack_Dodge reference = pendulum.GetComponentInChildren<Movement_Attack_Dodge>();
-            reference.speed = 1.5f;
-        }
-        else if (atkref.SpeedUpPendP1 == true && atkref.currentPlayer == 4)
-        {
-            Movement_Attack_Dodge reference = pendulum.GetComponentInChildren<Movement_Attack_Dodge>();
-            reference.speed = 1.5f;
-        }
-        else if (atkref.SpeedUpPendP1 == true && atkref.currentPlayer == 5)
-        {
-            Movement_Attack_Dodge reference = pendulum.GetComponentInChildren<Movement_Attack_Dodge>();
-            reference.speed = 2.5f;
-            atkref.SpeedUpPendP1 = false;
-
-            if (atkref.is1sClicked == true)
-            {
-                check = true;
-            }
+            check2 = true;
         }
     }
  }
